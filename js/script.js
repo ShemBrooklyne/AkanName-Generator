4d94cbf257bc2ad5606d80e3b0702719ec3b081e
ShemBrooklyne <shemnyatti@gmail.com>
2020-07-17 10:30:27 +0300

    Commited Changes

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Akan logic-able
+
+var maleNames = {
+    Sunday: "Kwasi",
+    Monday: "Kwadwo",
+    Tuesday: "Kwabena",
+    Wednesday: "Kwaku",
+    Thursday: "Yaw",
+    Friday: "Kofi",
+    Saturday: "Kwame",
+}
+
+var femaleNames = {
+    Sunday: "Akosua",
+    Monday: "Adwoa",
+    Tuesday: "Abenaa",
+    Wednesday: "Akua",
+    Thursday: "Yaa",
+    Friday: "Afua",
+    Saturday: "Ama",
+}
